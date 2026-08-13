ee4b61d3ee39aacef98822104d4851b54fd671aa
Santiago Ondris <santiagonicolas2001@gmail.com>
2026-08-12 21:39:10 -0300

    nombres de las tablas correctos 2 vez

--- apps/watchman/migrations/00008_performance_indexes_and_retention.sql
+++ apps/watchman/migrations/00008_performance_indexes_and_retention.sql
@@ -4,11 +4,9 @@ CREATE INDEX IF NOT EXISTS error_groups_fingerprint_idx ON error_groups (fingerp
 CREATE INDEX IF NOT EXISTS error_groups_env_idx ON error_groups (environment);
 CREATE INDEX IF NOT EXISTS interpretations_created_idx ON interpretations (created_at DESC);
 CREATE INDEX IF NOT EXISTS interpretations_model_idx ON interpretations (model);
-CREATE INDEX IF NOT EXISTS deployments_app_time_idx ON deployments (application, deployed_at DESC);
 
 -- +goose Down
 
-DROP INDEX IF EXISTS deployments_app_time_idx;
 DROP INDEX IF EXISTS interpretations_model_idx;
 DROP INDEX IF EXISTS interpretations_created_idx;
 DROP INDEX IF EXISTS error_groups_env_idx;
